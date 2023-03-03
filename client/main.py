260837e8b8fe9860a0765eab9a786cfba2b66b50
damaric <120008852+damaric@users.noreply.github.com>
2023-03-03 12:03:58 +0100

    Call GUI Functins
    Edit the code to call the Functions from GUI

--- client/main.py
+++ client/main.py
@@ -1,22 +1,32 @@
 import socket
 import network.handle_msg as handle_msg 
-
-def server():
-    HOST = 'localhost'
-    PORT = 12345
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
-    file = sock.makefile("r")
+import gui.main 
+from gui.main import MainWindow
+import threading
 
 
-    while True:
-        message = file.readline().strip()
-        
-        if not message:
-            print("Connection closed")
-            break
-        handle_msg.handle_msg(message)
-        
+class  Client:
+    def __init__(self):
+        threading.Thread = 
+        #TODO: Run connect_server in a other thread
+        gui = MainWindow
+        #
+    def connect_server():
+        HOST = 'localhost'
+        PORT = 12345
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        file = sock.makefile("r")
+
+
+        while True:
+            message = file.readline().strip()
+            
+            if not message:
+                print("Connection closed")
+                break
+            handle_msg.handle_msg(message)
+            
 
 
         
