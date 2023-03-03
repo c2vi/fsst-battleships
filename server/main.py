4abee6b530d51c7bdbf887590eb455b29e092d07
Thomas1q <thschmid@tsn.at>
2023-03-03 16:46:00 +0100

    Added PLayer class and handle Message

--- server/main.py
+++ server/main.py
@@ -4,12 +4,30 @@ import json
 from handle_msg import handle_msg
 
 
-def handle_socket(self,Id,player):
-    sock_file = self.server.makefile()
-    msg = (json.dumps({"msg": player, "Id": Id}) + "\n")
-    sock_file.write(msg)
+def check_client_data(rec):
+    message = rec["msg"]
+
+    if message == :
+
+
+
+
+
+def handle_socket(data):
+    sock_file = data.server.makefile()
+    msg = (json.dumps({"msg": "player_list", "Id": data.Id}) + "\n")
+    sock_file.send(msg)
     for line in sock_file:
-        handle_msg(json.loads(line))
+        try:
+            rec = handle_msg(json.loads(line))
+            check_client_data(rec)
+        except:
+            msg = "what you sent was not valid JSON"
+            sock_file.send(msg)
+
+
+
+
 
 class Server():
     def __init__(self):
@@ -23,38 +41,40 @@ class Server():
 
 
     def start_server(self):
+        self.Id = 0
+        self.players = {}
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(self.ADDR)
         self.server.listen(10)
         while True:
             self.conn, self.addr = self.server.accept()
-            thread = threading.Thread()
+            player = Player(self.Id,self.conn)
+            self.players[self.Id] = player
+            thread = threading.Thread(target=handle_socket, args=(self))
             thread.start()
+            self.Id = self.Id + 1
 
 
 
 
-class Player(Server):
-    def __init__(self):
+class Player():
+
+    def set_name(self,name):
+        self.player_name = name
+        return self.player_name
+    def __init__(self,Id,conn):
         super().__init__()
+        self.Id = Id
+        self.conn = conn
+        self.player_name = None
 
-    def player_name(self):
-        self.player_name = "set_name(name)"
 
-    def get_player_conn(self):
-        self.Id = {}
-        self.player={}
-        self.counter = 0
-        #player_name()
-        #self.player[self.player_name()] = self.player_name()
-        self.Id[self.counter] = self.counter
-        handle_socket(self.Id,self.player)
+#class game():
 
 
 
 
 
 if __name__ == '__main__':
-    player = Player()
     server = Server()
     server.start_server()
