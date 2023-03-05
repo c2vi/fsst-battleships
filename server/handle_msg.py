51f093bf8937b8274515341a5e2074d6684c6fdc
Thomas1q <thschmid@tsn.at>
2023-03-05 22:10:08 +0100

    Added all functions with an pass in it

--- server/handle_msg.py
+++ server/handle_msg.py
@@ -1,2 +1,43 @@
-def handle_msg(msg):
-    print(msg)
+
+
+def player_list():
+    pass
+
+def set_name():
+    pass
+
+def match_req_cancel():
+    pass
+
+def match_ack():
+    pass
+
+def match_deny():
+    pass
+
+def game_start():
+    pass
+
+def game_cancel():
+    pass
+
+def game_place():
+    pass
+
+def game_place_invalid():
+    pass
+
+def game_do_hit():
+    pass
+
+def game_hit():
+    pass
+
+def game_hit_success():
+    pass
+
+def set_score():
+    pass
+
+def error():
+    pass
