81c9c88d70814f11cbac98cc249365b3815ac1ea
Thomas1q <thschmid@tsn.at>
2023-03-19 21:17:58 +0100

    Ship placement almost working

--- client/gui/placeships.py
+++ client/gui/placeships.py
@@ -13,7 +13,9 @@ from PyQt6.QtWidgets import (
 class PlaceShips(QWidget):
     def __init__(self, main):
         super().__init__()
-
+        self.selected_ship = None
+        self.selected_x = None
+        self.selected_y = None
         self.main = main
         self.main.state = "place-ships"
 
@@ -26,15 +28,21 @@ class PlaceShips(QWidget):
         self.button_done = QPushButton("DONE")
         self.button_done.clicked.connect(self.button_done_clicked)
 
+
         enemy_field_layout = QGridLayout()
 
         own_field_layout = QGridLayout()
-
+        self.own_field_Buttons = []
         for x in range(1, 13):
             for y in range(1, 13):
                 own_field_button = QPushButton()
-                own_field_button.clicked.connect(self.own_button_clicked(own_field_button))
                 own_field_layout.addWidget(own_field_button, x, y)
+                own_field_button.clicked.connect(self.place_ship(x,y))
+                own_field_button.x = x
+                own_field_button.y = y
+
+
+                self.own_field_Buttons.append(own_field_button)
 
         own_field_widget = QWidget()
         own_field_widget.setLayout(own_field_layout)
@@ -51,82 +59,43 @@ class PlaceShips(QWidget):
         self.ships = QWidget()
         self.ships.setLayout(self.shiplayout)
 
-        def Zweierschiff():
-            shippiece1 = QPushButton()
-            shippiece2 = QPushButton()
-
-            DrehenZweierschiff = QPushButton("Rotate")
-
-            shippiece1.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece2.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-
-            Zweierschiffwidget = QWidget()
-            Zweierschifflayout = QGridLayout()
-
-            Zweierschifflayout.addWidget(DrehenZweierschiff,0,0)
-            Zweierschifflayout.addWidget(shippiece1, 1, 0)
-            Zweierschifflayout.addWidget(shippiece2, 2, 0)
-
-            Zweierschiffwidget.setLayout(Zweierschifflayout)
-
-            return Zweierschiffwidget
-
-        def Flugzeugträger():
-            shippiece1 = QPushButton()
-            shippiece2 = QPushButton()
-            shippiece3 = QPushButton()
-            shippiece4 = QPushButton()
-
-            DrehenFlugzeugträger = QPushButton("Rotate")
-
-            shippiece1.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece2.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece3.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece4.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
 
 
-            Flugzeugträgerwidget = QWidget()
-            Flugzeugträgerlayout = QGridLayout()
+        self.ship_map_flug = [
+            ["X","X"," "," "," "," "," "],
+            [" ","X","X"," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+        ]
+        self.ship_map_zweier = [
+            ["X","X"," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "],
+        ]
+        self.ship_map_dreier = [
+            ["X", "X", "X", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " "]
+        ]
 
-            Flugzeugträgerlayout.addWidget(DrehenFlugzeugträger,0,0)
-            Flugzeugträgerlayout.addWidget(shippiece1, 1, 0)
-            Flugzeugträgerlayout.addWidget(shippiece2, 2, 0)
-            Flugzeugträgerlayout.addWidget(shippiece3, 2, 1)
-            Flugzeugträgerlayout.addWidget(shippiece4, 3, 1)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_flug), 1, 0)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_zweier), 1, 2)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_zweier), 1, 3)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_zweier), 1, 4)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_dreier), 6, 1)
+        self.shiplayout.addWidget(self.make_ship(self.ship_map_dreier), 6, 2)
 
-            Flugzeugträgerwidget.setLayout(Flugzeugträgerlayout)
-
-            return Flugzeugträgerwidget
-
-        def Dreierschiff():
-            shippiece1 = QPushButton()
-            shippiece2 = QPushButton()
-            shippiece3 = QPushButton()
-
-            DrehenDreierschiff = QPushButton("Rotate")
-
-
-            shippiece1.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece2.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-            shippiece3.setStyleSheet('QPushButton {background-color: #A3C1DA; color: blue;}')
-
-            Dreierschiffwidget = QWidget()
-            Dreierschifflayout = QGridLayout()
-
-            Dreierschifflayout.addWidget(DrehenDreierschiff, 0, 0)
-            Dreierschifflayout.addWidget(shippiece1, 1, 0)
-            Dreierschifflayout.addWidget(shippiece2, 2, 0)
-            Dreierschifflayout.addWidget(shippiece3, 3, 0)
-
-            Dreierschiffwidget.setLayout(Dreierschifflayout)
-            return Dreierschiffwidget
-
-        self.shiplayout.addWidget(Flugzeugträger(), 1, 0)
-        self.shiplayout.addWidget(Zweierschiff(), 1, 2)
-        self.shiplayout.addWidget(Zweierschiff(), 1, 3)
-        self.shiplayout.addWidget(Zweierschiff(), 1, 4)
-        self.shiplayout.addWidget(Dreierschiff(), 6, 1)
-        self.shiplayout.addWidget(Dreierschiff(), 6, 2)
 
 
 
@@ -146,6 +115,59 @@ class PlaceShips(QWidget):
         game_layout.addWidget(self.ships, 0, 1)
 
         self.setLayout(game_layout)
+
+    def Offset(self, ship, y, x):
+        def Inner():
+            print("Offset",ship.map)
+            self.selected_ship = ship
+            self.selected_x = x
+            self.selected_y = y
+
+        return Inner
+
+    def make_ship(self, ship_map):
+        ship = QWidget()
+        ship_layout = QGridLayout()
+        ship.map = ship_map
+
+        for x in range(0, 6):
+            for y in range(0, 6):
+                ship_piece = QPushButton()
+                if ship_map[x][y] == "X":
+
+                    ship_layout.addWidget(ship_piece, y, x)
+                    ship_piece.clicked.connect(self.Offset(ship, y, x))
+
+        ship.setLayout(ship_layout)
+
+        return ship
+
+    def place_ship(self,x,y):
+
+        def inner():
+            print("inner")
+            absolut_x = x - self.selected_x
+            absolut_y = y - self.selected_y
+            for posx in range(0, 6):
+                for posy in range(0, 6):
+                    print("forloop")
+                    new_x = absolut_x + posx
+                    new_y = absolut_y + posy
+
+                    print(self.selected_ship.map,posx,posy)
+
+                    if self.selected_ship.map[posx][posy] == "X":
+                        print("if selected")
+                        self.main.placeships_array[posx][posy] = "X"
+                        for Button in self.own_field_Buttons:
+                            print("for Button")
+                            if new_x == Button.x and new_y == Button.y:
+                                Button.setStyleSheet("background-color:blue")
+
+
+
+
+        return inner
 
     def button_playerlist_clicked(self):
         self.main.state = "playerlist"
